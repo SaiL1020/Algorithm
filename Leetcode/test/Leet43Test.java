d3d77b5ba08d8b89da0b4b2ffd8d6e409e76440a
Sai <lisai1020@gmail.com>
2016-05-23 13:53:14 -0400

    Leetcode 43

---
+++ Leetcode/test/Leet43Test.java
@@ -0,0 +1,32 @@
+/**
+ * Created by sail on 5/22/16.
+ * Code one more line :)
+ */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Leet43Test {
+    @Test(timeout=2000)
+    public void test0() {
+        String n1 = "102", n2 = "102";
+        assertEquals("10404", new Leet43().multiply(n1, n2));
+    }
+
+    @Test(timeout=2000)
+    public void test1() {
+        String n1 = "12", n2 = "3";
+        assertEquals("36", new Leet43().multiply(n1, n2));
+    }
+
+    @Test(timeout=2000)
+    public void test2() {
+        String n1 = "12", n2 = "30";
+        assertEquals("360", new Leet43().multiply(n1, n2));
+    }
+    @Test(timeout=2000)
+    public void test3() {
+        String n1 = "102", n2 = "1001";
+        assertEquals("102102", new Leet43().multiply(n1, n2));
+    }
+}
+
