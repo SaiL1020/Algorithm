7dc931deef603d557f3a8cab048e697367d35875
Sai <lisai1020@gmail.com>
2016-06-14 23:30:09 -0400

    Leetcode 210 Course Schedule II

---
+++ Leetcode/src/Leet210.java
@@ -0,0 +1,52 @@
+/**
+ * Created by sail on 6/14/16.
+ * Code one more line :)
+ */
+import java.util.*;
+public class Leet210 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[][] degree = new int[numCourses][2]; // 0 in; 1 out
+        HashMap<Integer, List<Integer>> hm = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < prerequisites.length; i++) {
+            int in = prerequisites[i][0];
+            int out = prerequisites[i][1];
+            degree[in][0]++;
+            degree[out][1]++;
+            List<Integer> list;
+            if (!hm.containsKey(out)) {
+                list = new ArrayList<Integer>();
+            } else {
+                list = hm.get(out);
+            }
+            list.add(in);
+            hm.put(out, list);
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        List<Integer> hs = new ArrayList<Integer>();
+        for (int i = 0; i < numCourses; i++)
+            hs.add(i);
+        while (!hs.isEmpty()) {
+            boolean hasZeroIn = false;
+            for (int i = 0; i < hs.size(); i++) {
+                int in = hs.get(i);
+                if (degree[in][0] == 0) {
+                    hasZeroIn = true;
+                    ret.add(in);
+                    hs.remove(new Integer(in));
+                    List<Integer> list = hm.get(in);
+                    if (list == null) break;
+                    for (int j = 0; j < list.size(); j++) {
+                        int out = list.get(j);
+                        degree[out][0]--;
+                    }
+                }
+            }
+            if(!hasZeroIn)
+                break;
+        }
+        int[] arr = new int[0];
+        if (hs.isEmpty())
+            arr = ret.stream().mapToInt(i -> i).toArray();
+        return arr;
+    }
+}
---
+++ Leetcode/test/Leet210Test.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by sail on 6/14/16.
+ * Code one more line :)
+ */
+public class Leet210Test {
+    @Test(timeout=2000)
+    public void test0() {
+        int numCourses = 4;
+        int[][] preq = new int[][]{/*{1,0},{2,0},{3,1},{0,3}*/};
+        int[] ans = new Leet210().findOrder(numCourses, preq);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();
+        System.out.println(new int[]{0,1}.equals(new Leet210().findOrder(numCourses, preq)));
+    }
+}
