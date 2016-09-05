54a4cc322db8ec68b6e6c5d07cd3b3b8e3a12ee1
Sai <lisai1020@gmail.com>
2016-09-04 23:55:43 -0400

    9.4 Memories

---
+++ Leetcode/src/Leet152.java
@@ -0,0 +1,21 @@
+/**
+ * Created by sail on 9/4/16.
+ * Code one more line :)
+ */
+public class Leet152 {
+    public int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int tmp = max;
+            max = Math.max(nums[i], Math.max(tmp * nums[i], min * nums[i]));
+            min = Math.min(nums[i], Math.min(tmp * nums[i], min * nums[i]));
+            ans = Math.max(ans, max);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int ans = new Leet152().maxProduct(new int[]{2,-5,-2,-4,3});
+        System.out.println(ans);
+    }
+}
